676e6c0ae8eb0c7c4106375d0d33da6dbfa7f9d6
Prima Putra Agusantara <primatara42@gmail.com>
2023-01-04 18:18:50 +0700

    #Progress Main Acitivity

---
+++ app/src/main/java/com/tempatwisatapalembang/DetailActivity.java
@@ -0,0 +1,6 @@
+package com.tempatwisatapalembang;
+
+import android.app.Activity;
+
+public class DetailActivity extends Activity {
+}
--- app/src/main/java/com/tempatwisatapalembang/MainActivity.java
+++ app/src/main/java/com/tempatwisatapalembang/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rvWisata = findViewById(R.id.rv_wisata);
+        //rvWisata = findViewById(R.id.rv_wisata);
         rvWisata.setHasFixedSize(true);
         list.addAll(WisataData.getListData());
     }
