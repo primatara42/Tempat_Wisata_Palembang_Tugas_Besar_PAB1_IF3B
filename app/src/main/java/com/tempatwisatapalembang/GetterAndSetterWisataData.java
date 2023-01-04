3134107b627e4c3fdbd1fee825724fcbc14de9ac
Prima Putra Agusantara <primatara42@gmail.com>
2023-01-04 09:29:20 +0700

    #Add Java Class Getter & Setter Wisata Data

---
+++ app/src/main/java/com/tempatwisatapalembang/GetterAndSetterWisataData.java
@@ -0,0 +1,59 @@
+package com.tempatwisatapalembang;
+
+public class GetterAndSetterWisataData
+{
+    private String nameWisata;
+    private String descWisata;
+    private String alamatWisata;
+    private String jamOperasional;
+    private String tarif;
+    private double rating;
+
+    public String getNameWisata() {
+        return nameWisata;
+    }
+
+    public void setNameWisata(String nameWisata) {
+        this.nameWisata = nameWisata;
+    }
+
+    public String getDescWisata() {
+        return descWisata;
+    }
+
+    public void setDescWisata(String descWisata) {
+        this.descWisata = descWisata;
+    }
+
+    public String getAlamatWisata() {
+        return alamatWisata;
+    }
+
+    public void setAlamatWisata(String alamatWisata) {
+        this.alamatWisata = alamatWisata;
+    }
+
+    public String getJamOperasional() {
+        return jamOperasional;
+    }
+
+    public void setJamOperasional(String jamOperasional) {
+        this.jamOperasional = jamOperasional;
+    }
+
+    public String getTarif() {
+        return tarif;
+    }
+
+    public void setTarif(String tarif) {
+        this.tarif = tarif;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
