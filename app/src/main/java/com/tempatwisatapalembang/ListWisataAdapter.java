695dfa04ada222036c4fbdeeb28793bee3ae8eba
Prima Putra Agusantara <primatara42@gmail.com>
2023-01-04 15:32:16 +0700

    #Adapter Hampir Selesai

--- app/src/main/java/com/tempatwisatapalembang/ListWisataAdapter.java
+++ app/src/main/java/com/tempatwisatapalembang/ListWisataAdapter.java
@@ -1,8 +1,61 @@
 package com.tempatwisatapalembang;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ListWisataAdapter extends RecyclerView.Adapter<ListWisataAdapter>
-{
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
+import java.util.ArrayList;
+
+public class ListWisataAdapter extends RecyclerView.Adapter<ListWisataAdapter.ListViewHolder>
+{
+    private ArrayList<GetterAndSetterWisataData> listWisata;
+    public ListWisataAdapter(ArrayList<GetterAndSetterWisataData> list)
+    {
+        this.listWisata = list;
+    }
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        GetterAndSetterWisataData wisata = listWisata.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(wisata.getFotoWisata())
+                .apply(new RequestOptions().override(55, 55))
+                .into(holder.imgWisata);
+        holder.tvName.setText(wisata.getNameWisata());
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return listWisata.size();
+    }
+
+    class ListViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView imgWisata;
+        TextView tvName;
+
+
+        ListViewHolder(View view)
+        {
+            super(view);
+            imgWisata = itemView.findViewById(R.id.iv_foto);
+            tvName = itemView.findViewById(R.id.tv_nama);
+        }
+    }
 }
--- app/src/main/java/com/tempatwisatapalembang/MainActivity.java
+++ app/src/main/java/com/tempatwisatapalembang/MainActivity.java
@@ -1,14 +1,30 @@
 package com.tempatwisatapalembang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvWisata;
+    private ArrayList<GetterAndSetterWisataData> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvWisata = findViewById(R.id.rv_wisata);
+        rvWisata.setHasFixedSize(true);
+        list.addAll(WisataData.getListData());
+    }
+    private void showRecyclerList()
+    {
+        rvWisata.setLayoutManager(new LinearLayoutManager(this));
+        ListWisataAdapter listWisataAdapter = new ListWisataAdapter(list);
+        rvWisata.setAdapter(listWisataAdapter);
     }
 }
